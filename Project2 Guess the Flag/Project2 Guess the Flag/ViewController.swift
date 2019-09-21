e494aa8068e93245e53e644de2d6394ab29570cf
Jason Pinlac <jasonpinlacdev@gmail.com>
2019-09-21 10:25:49 -0400

    flags and autolayout

---
+++ Project2 Guess the Flag/Project2 Guess the Flag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project2 Guess the Flag
+//
+//  Created by Jason Pinlac on 9/20/19.
+//  Copyright © 2019 Jason Pinlac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
