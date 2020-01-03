bc50bdda689c93c0c2c3f8854effccc74163c532
Jason Pinlac <jasonpinlac@Jasons-MacBook-Air.local>
2020-01-02 21:04:52 -0500

    start of project 8 swifty words for iphone

---
+++ Projects/Project8 Swift Words iPhone/Project8 Swift Words iPhone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project8 Swift Words iPhone
+//
+//  Created by Jason Pinlac on 1/2/20.
+//  Copyright © 2020 Jason Pinlac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
