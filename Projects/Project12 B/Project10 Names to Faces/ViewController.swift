f6f13e1a25b752b6dc6f71168d9aaeb9c92e81ac
Jason Pinlac <jasonpinlac@Jasons-MacBook-Air.local>
2020-01-15 00:31:06 -0500

    start

---
+++ Projects/Project12 B/Project10 Names to Faces/ViewController.swift
@@ -0,0 +1,117 @@
+//
+//  ViewController.swift
+//  Project10 Names to Faces
+//
+//  Created by Jason Pinlac on 1/14/20.
+//  Copyright © 2020 Jason Pinlac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var people = [Person]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraButton = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(addNewPersonByCamera))
+            navigationItem.leftBarButtonItems = [addButton, cameraButton]
+        } else {
+            navigationItem.leftBarButtonItem = addButton
+        }
+    }
+    
+    @objc func addNewPerson() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    @objc func addNewPersonByCamera() {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+         guard let image = info[.editedImage] as? UIImage else { return }
+         let imageName = UUID().uuidString
+         let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+         if let jpegData = image.jpegData(compressionQuality: 0.8)  {
+             try? jpegData.write(to: imagePath)
+         }
+         let person = Person(name: "Unknown", image: imageName)
+         people.append(person)
+         collectionView.reloadData()
+         dismiss(animated: true)
+     }
+     
+     // function to get applications document directory
+     func getDocumentsDirectory() -> URL {
+         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+         return paths[0]
+     }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue a PersonCell")
+        }
+        let person = people[indexPath.item]
+        let path = getDocumentsDirectory().appendingPathComponent(person.image)
+        cell.imageView.image = UIImage(contentsOfFile: path.path)
+        cell.imageView.layer.cornerRadius = 3
+        cell.imageView.layer.borderWidth = 2
+        cell.imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
+        cell.nameLabel.text = person.name
+        cell.layer.cornerRadius = 7
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+           return people.count
+       }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let ac = UIAlertController(title: "What would you like to do?", message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Rename", style: .default) {
+            [weak self] _ in
+            self?.renamePerson(at: indexPath)
+        })
+        ac.addAction(UIAlertAction(title: "Delete", style: .default) {
+            [weak self] _ in
+            self?.deletePerson(at: indexPath)
+        })
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    func renamePerson(at indexPath: IndexPath) {
+        let ac = UIAlertController(title: "Rename Person", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        ac.addAction(UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak ac] _ in
+            guard let newName = ac?.textFields?[0].text else { return }
+            self?.people[indexPath.item].name = newName
+            self?.collectionView.reloadData()
+        })
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    func deletePerson(at indexPath: IndexPath) {
+        let ac = UIAlertController(title: "Are you sure?", message: "Once deleted it will be gone forever.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Yes", style: .default) {
+            [weak self] _ in
+            self?.people.remove(at: indexPath.item)
+            self?.collectionView.reloadData()
+        })
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # Hacking-with-Swift
+
+This repo contains completed applications I've put together using Paul Hudson's "100 Days of Swift" course. Outlined below are each of the applications followed by a short description of the concepts being applied within the app.
+
+Project 1: Storm Viewer
+Constants and variables, UITableView, UIImageView, FileManager, storyboards
+Get started coding in Swift by making an image viewer app and learning key concepts.
+
+Project 2: Guess the Flag
+Asset catalogs, UIButton, CALayer, UIColor, UIAlertController
+Make a game using UIKit, and learn about integers, buttons, colors and actions.
+
+Project 3: Social Media
+UIBarButtonItem, UIActivityViewController, URL
+Let users share to Facebook and Twitter by modifying project 1.
+
+Project 4: Easy Browser
+loadView(), WKWebView, URLRequest, UIToolbar, UIProgressView, key-value observing
+Embed Web Kit and learn about delegation, KVO, classes and UIToolbar.
+
+Project 5: Word Scramble
+Closures, method return values, booleans, NSRange
+Create an anagram game while learning about closures and booleans.
+
+Project 6: Auto Layout
+NSLayoutConstraint, Visual Format Language, layout anchors
+Get to grips with Auto Layout using practical examples and code.
+
+Project 7: Whitehouse Petitions
+JSON, Data, UITabBarController
+Make an app to parse Whitehouse petitions using JSON and a tab bar.
+
+Project 8: 7 Swifty Words
+addTarget(), enumerated(), count(), index(of:), joined(), property observers, range operators
+Build a word-guessing game and master strings once and for all.
+
+Project 9: Grand Central Dispatch
+DispatchQueue, perform(inBackground:)
+Learn how to run complex tasks in the background with GCD.
+
+Project 10: Names to Faces
+UICollectionView, UIImagePickerController, UUID, classes
+Get started with UICollectionView and the photo library.
+
+Project 11: Pachinko
+SpriteKit, physics, blend modes, radians, CGFloat, NSKeyedUnarchiver
+Dive into SpriteKit to try your hand at fast 2D games.
+
+Project 12: UserDefaults
+UserDefaults, NSCoding, Codable, NSKeyedArchiver
+Learn how to save user settings and data for later use.
